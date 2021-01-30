f7453e1522d7d15d037cda4bf1a136536de7b7ac
saaz181 <saliaz.mg326@gmail.com>
2021-01-31 01:11:59 +0330

    Change time format and input

--- rob.py
+++ rob.py
@@ -6,6 +6,11 @@ import datetime as dt
 import logging
 from tkinter import *
 from tkinter import messagebox
+import sys
+import os
+import threading
+from tsetmc import Time
+
 """
 This program can be initial multiple bots together but if only we open new program for it
 like we click on the "robot.py" and open two terminal and in which we type our username & password
@@ -17,6 +22,15 @@ ALGORITHM:
     3 - we must make sure that as the bot buys/sells the stock doesn't buy/sell it again
     
 """
+
+
+# For .exe file
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
 # Using logging module to see what error did we encountered
 logging.basicConfig(filename='robot.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -30,8 +44,6 @@ def clear():
 def clear_all():
     stock_price_entry.delete(0, END)
     stock_quantity_entry.delete(0, END)
-    start_time_entry.delete(0, END)
-    end_time_entry.delete(0, END)
 
 
 def buy():
@@ -52,8 +64,8 @@ def buy():
     else:
 
         try:
-            start_time = dt.datetime.strptime(start_time_entry.get(), "%H:%M")
-            end_time = dt.datetime.strptime(end_time_entry.get(), "%H:%M")
+            start_time = dt.datetime.strptime(start_time_entry.get(), "%H:%M:%S")
+            end_time = dt.datetime.strptime(end_time_entry.get(), "%H:%M:%S")
             time_bool = True
         except ValueError:
             time_bool = False
@@ -71,7 +83,7 @@ def buy():
         buy_message = messagebox.askokcancel(title="Price Check", message=f"از خرید خود به مبلغ {text} اطمینان دارید؟")
         if buy_message == True:
             clear_all()
-            call()
+            threading.Thread(target=call).start()
 
 
 def sell():
@@ -90,8 +102,8 @@ def sell():
         messagebox.showerror(title="Empty form field", message="لطقا همه فیلد هارا پر کنید")
 
     else:
-        start_time = dt.datetime.strptime(start_time_entry.get(), "%H:%M")
-        end_time = dt.datetime.strptime(end_time_entry.get(), "%H:%M")
+        start_time = dt.datetime.strptime(start_time_entry.get(), "%H:%M:%S")
+        end_time = dt.datetime.strptime(end_time_entry.get(), "%H:%M:%S")
 
         if stock_price != '' and stock_quantity != '' and int(stock_price) % 10 == 0:
             total_price = int(stock_price_entry.get()) * int(stock_quantity_entry.get())
@@ -101,14 +113,14 @@ def sell():
         sell_message = messagebox.askokcancel(title="Price Check", message=f"از فروش خود به مبلغ {text} اطمینان دارید؟")
         if sell_message == True:
             clear_all()
-            call()
+            threading.Thread(target=call).start()
 
 
 def ui():
     root = Tk()
     root.title("Stock Bot")
     root.resizable(False, False)
-    root.iconbitmap('bot.ico')
+    root.iconbitmap(resource_path('bot.ico'))
     root.geometry('400x400')
     user_label = Label(root, text="username", font=("Helvatica", 10), fg='red')
     user_label.grid(row=1, column=1, padx=50, pady=15)
@@ -145,7 +157,7 @@ def ui():
     stock_quantity_entry = Entry(root)
     stock_quantity_entry.grid(row=5, column=2, padx=70)
 
-    guid = Label(root, text="زمان را به صورت 23:23 وارد کنید", font=("Helvatica", 10, 'bold'), fg="red")
+    guid = Label(root, text="زمان را به صورت 23:23:23 وارد کنید", font=("Helvatica", 10, 'bold'), fg="red")
     guid.grid(row=6, column=1)
 
     start_time_label = Label(root, text="ساعت شروع", font=("Helvatica", 10))
@@ -180,8 +192,8 @@ def ui():
 
 # This function checks that our current time is in the our start time and our end time
 def time(start, end, current):
-    start = start.strftime("%H:%M")
-    end = end.strftime("%H:%M")
+    start = start.strftime("%H:%M:%S")
+    end = end.strftime("%H:%M:%S")
     if (start >= current) and (current <= end) and (start < end):
         return True
     return False
@@ -213,8 +225,8 @@ def log_in():  # Function which handles the log-in stuff
         # Passing password
         driver.find_element_by_xpath('//*[@id="Password"]').send_keys(password)
 
-
-        sleep(5)   # CAPTCHA time delay TODO: find better time limit if exists one
+        # CAPTCHA time delay
+        sleep(11)
 
         # clicking on log-in button
         driver.find_element_by_xpath('//*[@id="submit_btn"]').click()
@@ -226,16 +238,25 @@ def log_in():  # Function which handles the log-in stuff
         logging.error("Captcha didn't entered")
         driver.quit()
         call()
+
     try:
         # Passing through the junk pages
         driver.find_element_by_xpath('//*[@id="intro-mask"]/div[1]/div[13]').click()
-        sleep(1)
+        sleep(0.5)
         driver.find_element_by_xpath('//*[@id="intro-skip"]').click()
-        sleep(1)
+        sleep(0.5)
         driver.find_element_by_xpath('//*[@id="siteVersionContainer"]/div/div[1]/span[2]').click()
+
     except ElementNotInteractableException:
         logging.info("Slow Internet Connection -> couldn't load the main page")
         driver.quit()
+        call()
+
+    except NoSuchElementException:
+        logging.info("Wrong Password or username")
+        driver.quit()
+        messagebox.showwarning(title="CAPTCHA", message="Please enter the CAPTCHA next time")
+        # length -= 12
         call()
 
 
@@ -247,13 +268,13 @@ def stock_search():
         search_icon = _search.find_element_by_css_selector('#btnSearchStockAutoComplete > span')
 
         search_icon.click()
-        sleep(2)
+        sleep(1)
 
         # enter into our search field
         search = driver.find_element_by_xpath("//input[@placeholder='جستجوی سهم']")
 
         search.send_keys(stock)
-        sleep(1)
+        sleep(2)
 
         # Choosing our stock from dropdown box
         search.send_keys(Keys.ARROW_DOWN)
@@ -267,7 +288,6 @@ def stock_search():
         call()
 
 
-# **** main function which do the trade ***
 def trade():
     # website is automatically set to buy option so we just
     # need to define sell option
@@ -285,9 +305,19 @@ def trade():
     # passing price
     driver.find_element_by_xpath('//*[@id="send_order_txtPrice"]').send_keys(stock_price)
 
+    robot_trade()
+
+
+# **** main function which do the trade ***
+def robot_trade():
+    # How many times to click on buy/sell button
     length = (end_time - start_time).seconds
 
-    sleep(length)
+    now_time = Time()
+    # time to wait until time occurs
+    length_wait = (start_time - now_time.__str__()).seconds
+
+    sleep(length_wait - 2)
     for _ in range(length):
         # sell or buy button ** which the path is the same **
         sleep(0.3)
@@ -295,10 +325,11 @@ def trade():
 
         # press the final button
         sleep(0.3)
-        driver.find_element_by_xpath('//*[@id="sendorder_ModalConfirm_btnCancel"]').click()
-        if dt.datetime.now().time().strftime("%H:%M") == end_time:
-            break
+        driver.find_element_by_xpath('//*[@id="sendorder_ModalConfirm_btnSendOrder"]').click()
+
+    driver.quit()
     messagebox.showinfo(title="success", message="Trade completed")
+
 
 def start_trading():
     global driver
@@ -309,10 +340,13 @@ def start_trading():
     prefs = {"profile.default_content_setting_values.notifications": 2}
     # passing the argument to our chrome driver
     chrome_options.add_experimental_option("prefs", prefs)
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--allow-insecure-localhost')
+    chrome_options.add_argument('--ignore-ssl-errors=yes')
 
     # path to web driver location on PC TODO:make it dynamic
 
-    PATH = r"E:\IDMs\chromedriver.exe"
+    PATH = resource_path("chromedriver.exe")
     # starting driver
     driver = webdriver.Chrome(options=chrome_options, executable_path=PATH)
 
@@ -320,6 +354,7 @@ def start_trading():
     stock_search()  # Initializing the stock search function
 
 
+# call the trade function
 def call():
     # because in stock market price is from 10x
     if int(stock_price) % 10 != 0:
@@ -328,14 +363,11 @@ def call():
         clear()
     # we can't order buy under 5,000,000 RIALS
     elif (int(stock_price) * int(stock_quantity) < 5000000) and (trade_type == "b"):
-        messagebox.showerror(title="Underprice",
-                             message="You can't buy less than 5,000,000 ريال")
+        messagebox.showerror(title="Underprice", message="You can't buy less than 5,000,000 ريال")
         clear()
-
     else:
-
         # current time
-        current_time = dt.datetime.now().time().strftime("%H:%M")
+        current_time = Time().__str__().strftime("%H:%M:%S")
 
         # check if our current time is in the our start and end time
         if time(start_time, end_time, current_time):
---
+++ tsetmc.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import datetime as dt
+
+"""
+This class return TIME from http://www.tsetmc.com/Loader.aspx?ParTree=15 website
+in order to use it as our time option in bot
+"""
+
+
+class Time(object):
+    PATH = 'chromedriver.exe'
+
+    def __init__(self):
+        self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(executable_path=self.PATH, options=self.chrome_options)
+        self.driver.get("http://www.tsetmc.com/Loader.aspx?ParTree=15")
+
+    def __str__(self):
+        clock = self.driver.find_element_by_class_name('RealServerTime')
+        self.current_time = dt.datetime.strptime(clock.text, "%H:%M:%S")
+        return self.current_time
