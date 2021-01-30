64eebd85924b3416db3db00ba6f2d1c02d9e02ec
saaz181 <saliaz.mg326@gmail.com>
2021-01-31 02:46:21 +0330

    Remove headless from tsetmc.py

--- tsetmc.py
+++ tsetmc.py
@@ -12,12 +12,9 @@ class Time(object):
 
     def __init__(self):
         self.chrome_options = webdriver.ChromeOptions()
-        self.chrome_options.add_argument("--headless")  # make chrome browser hidden
         self.chrome_options.add_argument('--ignore-certificate-errors')  # disable chrome security alerts
-        self.chrome_options.add_argument('--allow-insecure-localhost')  # allow 'chromedriver' to run on localhost
-        self.chrome_options.add_argument('--ignore-ssl-errors=yes')  # disable SSL security of chrome browser
-
         self.driver = webdriver.Chrome(executable_path=self.PATH, options=self.chrome_options)
+        self.driver.minimize_window()
         self.driver.get("http://www.tsetmc.com/Loader.aspx?ParTree=15")
 
     def __str__(self):
